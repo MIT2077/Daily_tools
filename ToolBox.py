6b18e36676bbd72d69ac6444ec3b94cd37dd13e3
MIT2077 <omgusuchacock@gmail.com>
2023-05-05 14:34:50 +0300

    ToolBox. First 3 tools created. Patch 1. 14:30 05.05.2023.

---
+++ ToolBox.py
@@ -0,0 +1,13 @@
+class Tools:
+    def __init__(self):
+        pass
+
+    def get_vowels(String):
+        return [each for each in self if each in "AaEeIiOoUuYy"]
+
+    def capitalize(String):
+        return String.title()
+
+    def string_clone(String, Quantity):
+        return String * Quantity
+
